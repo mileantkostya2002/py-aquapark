0405b343a672e1ae21ed7c3ce2e098001a6961bb
Kostya Mileant <kostya.milya@gmail.com>
2025-05-01 12:20:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,24 +2,56 @@ from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount, max_amount):
+        self.min_amount = min_amount
+        self.max_amount = max_amount
 
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError
+
+        if not (self.min_amount <= value <= self.max_amount):
+            raise ValueError
 
 class Visitor:
-   pass
+    def __init__(self, name, age, weight, height):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
-
+    def __init__(self, age, weight, height):
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20,50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
-
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 class Slide:
-    pass
+    def __init__(self, name, limitation_class):
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor):
+        try:
+            self.limitation_class(visitor.age, visitor.wegit ight, visitor.height)
+            return True
+        except (ValueError, TypeError):
+            return False
